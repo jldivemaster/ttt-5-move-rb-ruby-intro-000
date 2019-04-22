454b212b0b105c15b1c18e2a5fb6d747cb378488
Jason Leach <jlspursfan@gmail.com>
2019-04-22 04:31:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,5 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
@@ -14,6 +14,6 @@ def input_to_index(position)
 end
 
 def move( board, index, token="X" )
-  board[index] << token.strip
+  board[index] << token
   return board
 end
